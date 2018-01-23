295c60e4af2e65c785f0f9c47b7cad1b6e9af390
Collin <rocket3989@gmail.com>
2018-01-23 16:43:29 -0600

    hey it works

---
+++ index.js
@@ -0,0 +1,17 @@
+var fs = require('fs')
+var marked = require('marked')
+var { exec, execSync } = require('child_process')
+var public = `${__dirname}/public`
+var source = `${__dirname}/source`
+
+
+var junk = fs.readdirSync(`${source}/blog`).forEach(htmlpost)
+
+function htmlpost(path){
+    var link = path.split('.')[0]
+    var markdown = fs.readFileSync(`${source}/blog/${path}`,'utf8')
+    var html = marked(markdown)
+    var dir = `${public}/${link}` 
+    if (!fs.existsSync(dir)) execSync(`mkdir -p ${dir}`)
+    fs.writeFileSync(`${dir}/index.html`, `<html>${html}</html>`)
+}
