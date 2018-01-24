ac0d6fb27fe9dc6e1cddec7b6b401b2dafc68465
Collin <rocket3989@gmail.com>
2018-01-23 18:03:29 -0600

    changed rsync

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var { exec, execSync } = require('child_process')
 var public = `${__dirname}/public`
 var source = `${__dirname}/source`
 function rsyncStatic(){
-    exec('rsync -a --exclude _posts/ --exclude _templates/ source/ public/')
+    exec('rsync -a --exclude blog/ source/ public/')
 }
 
 rsyncStatic()
